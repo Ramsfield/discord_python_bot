deaf67364be7b25bb92aad0cbfa3f62bdb77238b
Ramsfield <mgriffith004@mail.csuchico.edu>
2019-09-04 12:33:39 -0700

    Working version of news app

--- chicobot.py
+++ chicobot.py
@@ -27,8 +27,8 @@ class ChicoBot:
             print("Online")
             print(f"Name: {self.discord_client.user.name}")
             print(f"ID: {self.discord_client.user.id}")
-            yield from self.discord_client.change_presence(game=discord.Game(name='Chico Bot'))
-        
+            yield from self.discord_client.change_presence(game=discord.Game(name='Serving Chico Since 1985'))
+
         @self.discord_client.event
         @asyncio.coroutine
         def on_message(message):
@@ -41,6 +41,32 @@ class ChicoBot:
 
             if message.content.startswith(PREFIX):
                 yield from self.discord_client.process_commands(message)
+
+        ###SIMPLE HELLO COMMAND#####
+        @self.discord_client.command(description="Says Hello", brief="Hello", aliases=['h'], pass_context=True)
+        async def hello(context):
+            await self.discord_client.say("Hello")
+
+        ######NEWS COMMAND#########
+        @self.discord_client.command(description="Posts news and articles into news chat",
+                brief="News command",
+                aliases=['n'],
+                pass_context=True)
+        async def news(context):
+            channel_id = 373607739634745344
+            channels = [i for i in self.discord_client.get_all_channels()]
+            channel = None
+            for chan in channels:
+                if chan.name == "news-and-events":
+                    channel = chan
+
+            msg = ''.join([i + ' ' for i in context.message.content.split(" ")[1:]])
+            msg += f"by {context.message.author.mention} in {context.message.channel.name}"
+
+            if channel is None:
+                await self.discord_client.say(f"Failed to send to channel. Please try again or ping @ramsfield#7696 for assistance.")
+            else:
+                await self.discord_client.send_message(channel, content=f"{msg}")
 
     def run(self):
         self.discord_client.run(self.token)
